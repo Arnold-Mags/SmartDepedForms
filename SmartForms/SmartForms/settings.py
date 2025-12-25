7ce9ca74f7e6e4ef93bdf723fbe5992bd5c8c233
Arnold-Mags <112933384+Arnold-Mags@users.noreply.github.com>
2025-12-25 22:13:40 +0800

    updates_render

--- SmartForms/SmartForms/settings.py
+++ SmartForms/SmartForms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-+@62(skc)m=(o+d71t%i48n6%vdgv)i9$=3jeo65pst^+e*0p_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "smartdepedforms.onrender.com"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", #render
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -129,6 +130,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
